0dc433b260b586f639a44f1bac851740611f4825
Tristan Allal-Rimbaud <46636715+Baudrim@users.noreply.github.com>
2022-10-22 19:35:56 +0200

    Retroactive fix
    check if the cache is already a symlink in case where user already made the symlink before and somewhere else so we don't make copy of it and make a full mess

--- autoCache.sh
+++ autoCache.sh
@@ -3,16 +3,16 @@
 #TODO: Better text and color output
 #TODO: easier way to launch the script on the steam deck
 
-# The path to the cache folder
-COMPATDATA_PATH="/home/deck/.steam/steam/steamapps/compatdata"
-SHADERCACHE_PATH="/home/deck/.steam/steam/steamapps/shadercache"
+# # The path to the cache folder
+# COMPATDATA_PATH="/home/deck/.steam/steam/steamapps/compatdata"
+# SHADERCACHE_PATH="/home/deck/.steam/steam/steamapps/shadercache"
 
-# The path to the sd card and game path
-SD_PATH="/run/media/"
-# get the first folder in media, supposed to be the sd card
-SD_PATH=$(ls -d $SD_PATH*/ | head -n 1)
-GAME_PATH="$SD_PATH/steamapps/common"
-STEAMAPPS_PATH="$SD_PATH/steamapps/"
+# # The path to the sd card and game path
+# SD_PATH="/run/media/"
+# # get the first folder in media, supposed to be the sd card
+# SD_PATH=$(ls -d $SD_PATH*/ | head -n 1)
+# GAME_PATH="$SD_PATH/steamapps/common"
+# STEAMAPPS_PATH="$SD_PATH/steamapps/"
 
 # Steam api url
 STEAMAPI_URL="https://api.steampowered.com/ISteamApps/GetAppList/v2/"
@@ -60,44 +60,50 @@ for game in "$STEAMAPPS_PATH"/appmanifest_*.acf; do
 	fi
 	spacemb=$(($space/1024))
 	# check if the game actually exist in the compatdata folder and shadercache folder
-	echo -e $NC "The game $game_name is installed on the sd card" $NC
+	echo -e $NC "The game $game_name is installed on the sd card - Game ID: $game_id" $NC
 	# check if cache exist in the internal storage
 	if [ -d "$SHADERCACHE_PATH/$game_id" ] && [ -d "$COMPATDATA_PATH/$game_id" ]; then
-		# if mode is 0 then the cache is already on the sd card
-		if [ $mode -eq 0 ]; then
-			echo -e $BLUE "The cache of $game_name is already on the sd card" $NC
+		# check is the cache is already a symlink in case where user already made the symlink before and somewhere else
+		if [ -L "$SHADERCACHE_PATH/$game_id" ] || [ -L "$COMPATDATA_PATH/$game_id" ]; then
+			echo -e $ORANGE "The shadercache and/or compatdata of the game $game_name already has a symlink somewhere else..." $NC
 		else
-		# check if there is enough space on the sd card
-			if [ $(df -k "$SD_PATH" | awk 'NR==2 {print $4}') -gt $space ]; then
-				# ask user if he wants to move the cache to the sd card by pressing y or n
-				echo -e $ORANGE "The cache of $game_name will take $spacemb Mb on the sd card" $NC
-				read -p "Do you want to move the cache of $game_name to the sd card? (y/n) " -n 1 -r
-				echo
-				if [[ $REPLY =~ ^[Yy]$ ]]; then
-					case $mode in
-					# check if the sd card has enough space to move the cache 
-					1)
-						mv "$COMPATDATA_PATH/$game_id" "$SD_PATH/cache/compatdata/"
-						ln -s "$SD_PATH/cache/compatdata/$game_id" "$COMPATDATA_PATH"
-						echo -e $GREEN "The compatdata of $game_name has been moved to the sd card" $NC
-						;;
-					2)
-						mv "$SHADERCACHE_PATH/$game_id" "$SD_PATH/cache/shadercache/"
-						ln -s "$SD_PATH/cache/shadercache/$game_id" "$SHADERCACHE_PATH"
-						echo -e $GREEN "The shadercache of $game_name has been moved to the sd card" $NC
-						;;
-					3)
-						
-						mv "$COMPATDATA_PATH/$game_id" "$SD_PATH/cache/compatdata/"
-						ln -s "$SD_PATH/cache/compatdata/$game_id" "$COMPATDATA_PATH"
-						mv "$SHADERCACHE_PATH/$game_id" "$SD_PATH/cache/shadercache/"
-						ln -s "$SD_PATH/cache/shadercache/$game_id" "$SHADERCACHE_PATH"
-						echo -e $GREEN "The compatdata and shadercache of $game_name has been moved to the sd card" $NC
-						;;
-					esac
-				fi
+			# if mode is 0 then the cache is already on the sd card
+			if [ $mode -eq 0 ]; then
+				echo -e $BLUE "The cache of $game_name is already on the sd card" $NC
 			else
-				echo -e $RED "Not enough space on the sd card to move the cache $game_name need $spacemb Mb" $NC
+			# check if there is enough space on the sd card
+			#check if the game shader cache is already a symlink
+				if [ $(df -k "$SD_PATH" | awk 'NR==2 {print $4}') -gt $space ]; then
+					# ask user if he wants to move the cache to the sd card by pressing y or n
+					echo -e $ORANGE "The cache of $game_name will take $spacemb Mb on the sd card" $NC
+					read -p "Do you want to move the cache of $game_name to the sd card? (y/n) " -n 1 -r
+					echo
+					if [[ $REPLY =~ ^[Yy]$ ]]; then
+						case $mode in
+						# check if the sd card has enough space to move the cache 
+						1)
+							mv "$COMPATDATA_PATH/$game_id" "$SD_PATH/cache/compatdata/"
+							ln -s "$SD_PATH/cache/compatdata/$game_id" "$COMPATDATA_PATH"
+							echo -e $GREEN "The compatdata of $game_name has been moved to the sd card" $NC
+							;;
+						2)
+							mv "$SHADERCACHE_PATH/$game_id" "$SD_PATH/cache/shadercache/"
+							ln -s "$SD_PATH/cache/shadercache/$game_id" "$SHADERCACHE_PATH"
+							echo -e $GREEN "The shadercache of $game_name has been moved to the sd card" $NC
+							;;
+						3)
+							
+							mv "$COMPATDATA_PATH/$game_id" "$SD_PATH/cache/compatdata/"
+							ln -s "$SD_PATH/cache/compatdata/$game_id" "$COMPATDATA_PATH"
+							mv "$SHADERCACHE_PATH/$game_id" "$SD_PATH/cache/shadercache/"
+							ln -s "$SD_PATH/cache/shadercache/$game_id" "$SHADERCACHE_PATH"
+							echo -e $GREEN "The compatdata and shadercache of $game_name has been moved to the sd card" $NC
+							;;
+						esac
+					fi
+				else
+					echo -e $RED "Not enough space on the sd card to move the cache $game_name need $spacemb Mb" $NC
+				fi
 			fi
 		fi
 	else
