e8ba1f2b7e0396cf99299bb6993e9bd4c917302c
Tristan Allal-Rimbaud <46636715+Baudrim@users.noreply.github.com>
2022-12-01 20:48:09 +0100

    Multiple sd/ssd handeler
    Now you will be asked to select the disk on which you want to move cache (it will be the cache of this disk that will be created according to the games found on the disk in question)

--- autoCache.sh
+++ autoCache.sh
@@ -3,16 +3,49 @@
 #TODO: Better text and color output
 #TODO: easier way to launch the script on the steam deck
 
-# The path to the cache folder
+# The path to the local cache folder
 COMPATDATA_PATH="/home/deck/.steam/steam/steamapps/compatdata"
 SHADERCACHE_PATH="/home/deck/.steam/steam/steamapps/shadercache"
 
-# The path to the sd card and game path
-SD_PATH="/run/media/"
-# get the first folder in media, supposed to be the sd card
-SD_PATH=$(ls -d $SD_PATH*/ | head -n 1)
+declare -a DISKS
+DISKS=($(df -h --total | grep media | awk '{print $1, $6, $2, $4}'))
+SD_PATH=$(zenity --list --title="Select the disk" --text="Select the disk where you want to move the cache" --column="Disk" --column="Path" --column="Size" --column="Free space"  --print-column="2" "${DISKS[@]}")
+clear
+printf "You selected: |%s| \n" "$SD_PATH"
+# Check if the sd card has a steamapps folder
+if [ ! -d "$SD_PATH/steamapps" ]; then
+	#if there is a SteamLibrary folder change SD_PATH to the SteamLibrary folder
+	if [ -d "$SD_PATH/SteamLibrary" ]; then
+		SD_PATH="$SD_PATH/SteamLibrary"
+	fi
+fi
+
+if [ ! -d "$SD_PATH/steamapps" ]; then
+	zenity --question --width=700  --title="Steamapps folder missing" --text="The steamapps folder seems to be missing on the sd card, do you want to manually select steamapps folder ?"
+	if [ $? -eq 0 ]; then
+		SD_PATH=$(zenity  --file-selection --directory --title="Select the sd steamapps folder")
+		#remove /steamapps from the path
+		SD_PATH=$(echo "$SD_PATH" | sed 's/\/steamapps//g')
+		clear
+		printf "You selected: %s \n" "$SD_PATH"
+		# Check if the sd card has a steamapps folder
+		if [ ! -d "$SD_PATH/steamapps" ]; then
+			zenity --error --title="Steamapps folder missing" --text="The steamapps folder is missing on the sd card."
+			exit 1
+		fi
+	else
+		clear
+		printf "You didn't select a folder, exiting \n"
+		exit 1
+	fi
+fi
 GAME_PATH="$SD_PATH/steamapps/common"
 STEAMAPPS_PATH="$SD_PATH/steamapps/"
+#check if the steamapps folder is empty
+if [ ! "$(ls -A $STEAMAPPS_PATH)" ]; then
+	zenity --error --title="Steamapps folder empty" --text="The steamapps folder is empty on the sd card."
+	exit 1
+fi
 
 # Steam api url
 STEAMAPI_URL="https://api.steampowered.com/ISteamApps/GetAppList/v2/"
@@ -27,6 +60,7 @@ NC='\033[0m' # No Color
 
 declare -i mode=0
 declare -i space=0
+
 #Check if the sd card has a cache folder and if not create it
 if [ ! -d "$SD_PATH/cache" ]; then
     mkdir "$SD_PATH/cache"
@@ -53,11 +87,15 @@ for game in "$STEAMAPPS_PATH"/appmanifest_*.acf; do
 	# check if the compatdata and/or shadercache folder is already on the sd card
 	if [ ! -d "$SD_PATH/cache/compatdata/$game_id" ]; then
 		mode+=1
-		space+=$(du -sb "$COMPATDATA_PATH/$game_id" | cut -f1)
+		if [ -d "$COMPATDATA_PATH/$game_id" ]; then
+			space+=$(du -sb "$COMPATDATA_PATH/$game_id" | cut -f1)
+		fi
 	fi
 	if [ ! -d "$SD_PATH/cache/shadercache/$game_id" ]; then
 		mode+=2
-		space+=$(du -sb "$SHADERCACHE_PATH/$game_id" | cut -f1)
+		if [ -d "$SHADERCACHE_PATH/$game_id" ]; then
+			space+=$(du -sb "$SHADERCACHE_PATH/$game_id" | cut -f1)
+		fi
 	fi
 	#convert byte  size in human readable format
 	spacemb=$(numfmt --to=iec-i --suffix=B  $space)
@@ -79,9 +117,11 @@ for game in "$STEAMAPPS_PATH"/appmanifest_*.acf; do
 				if [ $space -lt $(df -B1 $SD_PATH | tail -n 1 | awk '{print $4}') ]; then
 					# ask user if he wants to move the cache to the sd card by pressing y or n
 					echo -e $ORANGE "The cache of $game_name will take $spacemb on the sd card" $NC
-					read -p "Do you want to move the cache of $game_name to the sd card? (y/n) " -n 1 -r
-					echo
-					if [[ $REPLY =~ ^[Yy]$ ]]; then
+					#Show a window to the user asking if he wants to move the cache to the sd card
+					zenity --question --width=700 --text="The cache of $game_name will take $spacemb on the sd card. Do you want to move it to the sd card?" --title="Move cache to sd card" 2> >(grep -v "" >&2)
+					#Check if the user pressed yes or no
+					if [ $? -eq 0 ]; then
+						# if mode is 1 then only the compatdata folder is not on the sd card
 						case $mode in
 						# check if the sd card has enough space to move the cache 
 						1)
