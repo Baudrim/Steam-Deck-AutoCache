c909479be6062e7cd7f9f28b325aef717b55402a
Tristan Allal-Rimbaud <46636715+Baudrim@users.noreply.github.com>
2022-12-02 12:31:27 +0100

    Some cleaning

--- autoCache.sh
+++ autoCache.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# autoCache2.sh - A version of autoCache.sh that uses zenity to display a GUI
 
 #font color
 RED='\033[0;31m'
@@ -19,7 +18,6 @@ DISKS=($(df -h --total | grep media | awk '{print $1, $6, $2, $4}'))
 SD_PATH=$(zenity --list --width=700  --title="Select the disk" --text="Select the disk where you want to move the cache" --column="Disk" --column="Path" --column="Size" --column="Free space"  --print-column="2" "${DISKS[@]}")
 clear
 printf "You selected: |%s| \n" "$SD_PATH"
-# Check if the sd card has a steamapps folder
 if [ ! -d "$SD_PATH/steamapps" ]; then
 	#if there is a SteamLibrary folder change SD_PATH to the SteamLibrary folder
 	if [ -d "$SD_PATH/SteamLibrary" ]; then
@@ -35,7 +33,6 @@ if [ ! -d "$SD_PATH/steamapps" ]; then
 		SD_PATH=$(echo "$SD_PATH" | sed 's/\/steamapps//g')
 		clear
 		printf "You selected: %s \n" "$SD_PATH"
-		# Check if the sd card has a steamapps folder
 		if [ ! -d "$SD_PATH/steamapps" ]; then
 			zenity --error --title="Steamapps folder missing" --text="The steamapps folder is missing on the sd card."
 			exit 1
@@ -48,23 +45,20 @@ if [ ! -d "$SD_PATH/steamapps" ]; then
 fi
 GAME_PATH="$SD_PATH/steamapps/common"
 STEAMAPPS_PATH="$SD_PATH/steamapps/"
-#check if the steamapps folder is empty
 if [ ! "$(ls -A $STEAMAPPS_PATH)" ]; then
 	zenity --error --title="Steamapps folder empty" --text="The steamapps folder is empty on the sd card."
 	exit 1
 fi
 
-#Check if the sd card has a cache folder and if not create it
+#Check if the different cache folders exist or create them
 if [ ! -d "$SD_PATH/cache" ]; then
     mkdir "$SD_PATH/cache"
 fi
 
-#Check if the sd card has a compatdata folder and if not create it
 if [ ! -d "$SD_PATH/cache/compatdata" ]; then
     mkdir "$SD_PATH/cache/compatdata"
 fi
 
-#Check if the sd card has a shadercache folder and if not create it
 if [ ! -d "$SD_PATH/cache/shadercache" ]; then
     mkdir "$SD_PATH/cache/shadercache"
 fi
@@ -74,12 +68,9 @@ declare -a GAMES
 for game in "$STEAMAPPS_PATH"/appmanifest_*.acf; do
     space=0
     spaceHuman=0
-    # get the game id
     game_id=$(basename "$game" | cut -d'_' -f2 | cut -d'.' -f1)
-    # get the game name
     game_name=$(grep -m 1 "name" "$game" | cut -d'"' -f4)
     if [ ! -L "$SHADERCACHE_PATH/$game_id" ] || [ ! -L "$COMPATDATA_PATH/$game_id" ]; then
-        #check if compatdata folder exists
         if  [ -d "$COMPATDATA_PATH/$game_id" ] && [ ! -L "$COMPATDATA_PATH/$game_id" ]; then
                 space+=$(du -sb "$COMPATDATA_PATH/$game_id" | cut -f1)
         fi
@@ -87,8 +78,6 @@ for game in "$STEAMAPPS_PATH"/appmanifest_*.acf; do
                 space+=$(du -sb "$SHADERCACHE_PATH/$game_id" | cut -f1)
         fi
         spaceHuman=$(numfmt --to=iec-i --suffix=B --padding=7 $space)
-        #Add the game to the array
-        # if size is 0 then don't add it to the array
         if [ $space -ne 0 ]; then
             GAMES+=("FALSE" "$game_id" "$game_name" "$spaceHuman")
         fi
@@ -98,18 +87,15 @@ done
 #zenity prompt user to choose the games cache to move
 GAMES_CHOSEN=$(zenity --list --width=700 --height=500 --title="Select the games" --text="Choose the games you want to move the cache in the selected disk (They are all games installed on this same disk)" --column="Move?" --column="Appid" --column="Game" --column="Cache size" --checklist --print-column="2" "${GAMES[@]}" --separator=" ")
 clear
-# convert the string to an array
 GAMES_CHOSEN=($GAMES_CHOSEN)
 LOCAL_SPACE=$(df -BM --total "/home/deck" | grep total | awk '{print $4}' | sed 's/M//g')
-#check if the user selected any games
+
 if [ -z "$GAMES_CHOSEN" ]; then
     zenity --error --title="No games selected" --text="You didn't select any games."
     exit 1
 else
-    #declare toPrint
     (
     toPrint="[RESULTS]\n"
-    #set gamepercent to 100 divided by the number of games
     gamepercent=$((100/${#GAMES_CHOSEN[@]}))
     printf "gamepercent: %s \n" "$gamepercent"
     i=0
@@ -141,7 +127,6 @@ else
         #Same for the shadercache
         if [ -d "$SHADERCACHE_PATH/$game" ]; then
             if [ ! -L "$SHADERCACHE_PATH/$game" ]; then
-                #check if there is enough space on the sd card to move the cache
                 if [ $(df -B1 "$SD_PATH" | tail -1 | awk '{print $4}') -gt $(du -sb "$SHADERCACHE_PATH/$game" | cut -f1) ]; then
                     mv "$SHADERCACHE_PATH/$game" "$SD_PATH/cache/shadercache/"
                     ln -s "$SD_PATH/cache/shadercache/$game" "$SHADERCACHE_PATH"
